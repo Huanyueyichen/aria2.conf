82d3c68ac8c009e0d2118ecb5111a1d65e064ca7
Huanyueyichen <44775776+Huanyueyichen@users.noreply.github.com>
2023-11-22 22:03:24 +0800

    Update tracker.sh

--- aria2-linux/tracker.sh
+++ aria2-linux/tracker.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 list=`wget -qO- https://cf.trackerslist.com/all_aria2.txt|awk NF|sed ":a;N;s/\n/,/g;ta"`
-if [ -z "`grep "bt-tracker" ./aria2.conf`" ]; then
-    sed -i '$a bt-tracker='${list} ./aria2.conf
-    echo add trackers successful
+if [ -z "`grep "bt-tracker=" ~/.aria2/aria2.conf`" ]; then
+    sed -i '$a bt-tracker='${list} ~/.aria2/aria2.conf
+    echo add trackerlist successful
 else
-    sed -i "s@bt-tracker.*@bt-tracker=$list@g" ./aria2.conf
-    echo update trackers successful
+    sed -i "s@bt-tracker=.*@bt-tracker=$list@g" ~/.aria2/aria2.conf
+    echo update trackerlist successful
 fi
 
 systemctl --user restart aria2.service
